8485adf64a6d2c7a8c0f3baeb93741f54f052b01
Lardjo <knikitanov@me.com>
2012-09-25 18:38:51 +0400

    Add Download XML File

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 import os
 import sys
-import urllib
+import urllib.request
 import pymongo
 
 from xml.dom.minidom import *
@@ -19,7 +19,35 @@ print ("SteamID...")
 print ("Enter your game login on Steam: ")
 name = input()
 steamxml = "http://steamcommunity.com/id/" + name +"?xml=1"
-print (steamxml)
-input()
 
 # END Input Steam username and write full path to XML file
+
+# Download XML User File
+
+file_name = (name + ".xml")
+u = urllib.request.urlopen(steamxml)
+f = open(file_name, "wb")
+meta = u.info()
+fsize = int(meta.get("Content-Length"))
+print ("Downloading system information. File Size: %s Bytes" % (fsize))
+
+fsize_dl = 0
+block_sz = 8192
+
+while True:
+
+	buffer = u.read(block_sz)
+	
+	if not buffer:
+		
+		break
+
+	fsize_dl += len(buffer)
+	f.write(buffer)
+	status = r"%10d [%3.2f%%]" % (fsize_dl, fsize_dl * 100. / fsize)
+	status = status + chr(8)*(len(status)+1)
+	print (status)
+
+f.close()
+
+# END Download XML User File
