4848c5ca91caaa274ab86f55a7229c4cd7967539
Lardjo <knikitanov@me.com>
2012-09-25 16:03:35 +0400

    Add bat file. Check username. XML

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 # File: main.py
 # Main File
 
+import os
+import sys
+import urllib
+import pymongo
+
+from xml.dom.minidom import *
+
+name = ""
+steamxml = ""
+steamjson = ""
+
+# Input Steam username and write full path to XML file
+
+print ("SteamID...")
+print ("Enter your game login on Steam: ")
+name = input()
+steamxml = "http://steamcommunity.com/id/" + name +"?xml=1"
+print (steamxml)
+input()
+
+# END Input Steam username and write full path to XML file
